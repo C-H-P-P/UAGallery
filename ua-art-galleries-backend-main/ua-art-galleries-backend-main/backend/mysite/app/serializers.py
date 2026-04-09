ac02d15029009a414ed3840b0db78397654c9f4a
C_H_P <kachandmytro20@gmail.com>
2026-04-09 21:25:24 +0300

    Restore backwards compatibility fields in serializer for frontend

--- ua-art-galleries-backend-main/ua-art-galleries-backend-main/backend/mysite/app/serializers.py
+++ ua-art-galleries-backend-main/ua-art-galleries-backend-main/backend/mysite/app/serializers.py
@@ -2,85 +2,105 @@ from rest_framework import serializers
 from .models import Gallery
 from django.contrib.auth.models import User
 
+class DynamicLocaleMixin:
+    """Міксин для динамічного вибору мови на основі ?lang= параметра в запиті"""
+    
+    def get_lang(self):
+        request = self.context.get('request')
+        if request and hasattr(request, 'query_params'):
+            return request.query_params.get('lang', 'uk')
+        return 'uk'
 
-class GalleryListSerializer(serializers.ModelSerializer):
+    def resolve_locale(self, obj, field_base):
+        lang = self.get_lang()
+        if lang == 'en':
+            return getattr(obj, f"{field_base}_en", '')
+        return getattr(obj, f"{field_base}_ua", '')
+
+    def get_name(self, obj): return self.resolve_locale(obj, 'name')
+    def get_city(self, obj): return self.resolve_locale(obj, 'city')
+    def get_address(self, obj): return self.resolve_locale(obj, 'address')
+    def get_short_description(self, obj): return self.resolve_locale(obj, 'short_description')
+    
+class GalleryListSerializer(DynamicLocaleMixin, serializers.ModelSerializer):
     """Серіалізатор для списку галерей (коротка інфо)"""
-    city_ua = serializers.CharField(source='city', read_only=True)
-    city_en = serializers.CharField(source='city', read_only=True)
-    address_ua = serializers.CharField(source='address', read_only=True)
-    address_en = serializers.CharField(source='address', read_only=True)
-    short_description_ua = serializers.CharField(source='short_description', read_only=True)
-    short_description_en = serializers.CharField(source='short_description', read_only=True)
+    name = serializers.SerializerMethodField()
+    city = serializers.SerializerMethodField()
+    address = serializers.SerializerMethodField()
+    short_description = serializers.SerializerMethodField()
 
     class Meta:
         model = Gallery
         fields = [
             'id',
-            'name_ua',
-            'name_en',
             'slug',
             'status',
-            'city_ua',
-            'city_en',
-            'address_ua',
-            'address_en',
+            'name',
+            'city',
+            'address',
             'image',
-            'short_description_ua',
-            'short_description_en',
+            'short_description',
             'founding_year',
             'social_links',
             'created_at',
             'updated_at',
+            # Залишаємо _ua/_en для повної сумісності
+            'name_ua', 'name_en', 'city_ua', 'city_en', 
+            'address_ua', 'address_en', 'short_description_ua', 'short_description_en'
         ]
 
 
-class GalleryDetailSerializer(serializers.ModelSerializer):
+class GalleryDetailSerializer(DynamicLocaleMixin, serializers.ModelSerializer):
     """Серіалізатор для деталей галереї (повна інфо)"""
-    full_description_ua = serializers.CharField(source='description_ua', read_only=True)
-    full_description_en = serializers.CharField(source='description_en', read_only=True)
-    specialization_ua = serializers.CharField(source='short_description_ua', read_only=True)
-    specialization_en = serializers.CharField(source='short_description_en', read_only=True)
-    website_url_ua = serializers.CharField(source='website_url', read_only=True)
-    website_url_en = serializers.CharField(source='website_url', read_only=True)
+    name = serializers.SerializerMethodField()
+    city = serializers.SerializerMethodField()
+    address = serializers.SerializerMethodField()
+    short_description = serializers.SerializerMethodField()
+    
+    description = serializers.SerializerMethodField()
+    full_description = serializers.SerializerMethodField()
+    founders = serializers.SerializerMethodField()
+    curators = serializers.SerializerMethodField()
+    artists = serializers.SerializerMethodField()
+    specialization = serializers.SerializerMethodField()
+
+    def get_description(self, obj): return self.resolve_locale(obj, 'description')
+    def get_full_description(self, obj): return self.resolve_locale(obj, 'description')
+    def get_founders(self, obj): return self.resolve_locale(obj, 'founders')
+    def get_curators(self, obj): return self.resolve_locale(obj, 'curators')
+    def get_artists(self, obj): return self.resolve_locale(obj, 'artists')
+    def get_specialization(self, obj): return self.resolve_locale(obj, 'short_description')
 
     class Meta:
         model = Gallery
         fields = [
             'id',
-            'name_ua',
-            'name_en',
             'slug',
             'status',
-            'city_ua',
-            'city_en',
-            'address_ua',
-            'address_en',
+            'name',
+            'city',
+            'address',
             'image',
-            'short_description_ua',
-            'short_description_en',
-            'description_ua',
-            'description_en',
-            'full_description_ua',
-            'full_description_en',
-            'founders_ua',
-            'founders_en',
-            'curators_ua',
-            'curators_en',
-            'artists_ua',
-            'artists_en',
+            'short_description',
+            'description',
+            'full_description',
+            'founders',
+            'curators',
+            'artists',
+            'specialization',
             'email',
             'phone',
             'website_url',
-            'website_url_ua',
-            'website_url_en',
-            'specialization_ua',
-            'specialization_en',
             'founding_year',
             'social_links',
             'created_at',
             'updated_at',
+            # Backwards compat manual fields
+            'name_ua', 'name_en', 'city_ua', 'city_en',
+            'address_ua', 'address_en', 'short_description_ua', 'short_description_en',
+            'description_ua', 'description_en', 'founders_ua', 'founders_en',
+            'curators_ua', 'curators_en', 'artists_ua', 'artists_en'
         ]
-
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
