fb0b1e2d4e75ae589df29d8d2937c8c7377e50d5
C_H_P <kachandmytro20@gmail.com>
2026-06-06 22:20:57 +0300

    fix: import force_bytes

--- ua-art-galleries-backend-main/ua-art-galleries-backend-main/backend/mysite/app/auth_views.py
+++ ua-art-galleries-backend-main/ua-art-galleries-backend-main/backend/mysite/app/auth_views.py
@@ -96,6 +96,7 @@ class MinimalRegisterView(APIView):
 
         from django.contrib.auth.tokens import default_token_generator
         from django.utils.http import urlsafe_base64_encode
+        from django.utils.encoding import force_bytes
         import requests
         import os
 
